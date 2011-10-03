9a505231657422ab69c777a87f76a7faf848ffeb
Jakub Pavelek <jpavelek@live.com>
2011-10-03 10:07:18 +0200

    Changes: Fixed the main.cpp to compile again.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QDeclarativeView *view = new QDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->show();
-    QObject::connect(view.engine(), SIGNAL(quit()), &view, SLOT(close()));
+    QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
 
     return app.exec();
 }
