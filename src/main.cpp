9bb09e69801efec84f98cbba5dc9ac8cb8b45f9a
Jakub Pavelek <jpavelek@live.com>
2011-10-06 13:28:41 +0200

    Changes: Depending on platform, run fullscreen on device and normal on desktop

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,16 @@
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setProperty("NoMStyle", true);
     QDeclarativeView *view = new QDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
+
+#ifdef __arm__
+    window.showFullScreen();
+#else
     view->show();
+#endif
+
     QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
 
     return app.exec();
