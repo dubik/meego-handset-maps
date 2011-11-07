c5d3533a3c524ea8226c9a6d7d355d3d1dcdcac8
Jakub Pavelek <jpavelek@live.com>
2011-11-07 16:25:42 +0100

    Changes: Get the declarative view from cache.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 #ifdef __arm__
     QApplication *app = MDeclarativeCache::qApplication(argc, argv);
     app->setProperty("NoMStyle", true);
-    QDeclarativeView *view = new QDeclarativeView();
+    QDeclarativeView *view = MDeclarativeCache::qDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->showFullScreen();
     QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
--- src/src.pri
+++ src/src.pri
@@ -3,11 +3,9 @@ SOURCES += src/main.cpp
 
 OTHER_FILES += \
     src/qml/main.qml \
-    src/qml/MainPage.qml \
-    src/data/meegohandsetmaps.desktop \
-    src/data/icon-m-current-position.svg \
-    src/data/icon-m-zoom-in.svg \
-    src/data/icon-m-zoom-out.svg
+    src/qml/AddWebApp.qml \
+    src/data/icon-m-add-application.svg \
+    src/data/icons-Application-html5.svg
 
 RESOURCES += \
     src/res.qrc
