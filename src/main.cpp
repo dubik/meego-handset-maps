76a91953afdbeb8a94e8ed44a881116be1215373
Jakub Pavelek <jpavelek@live.com>
2011-11-12 22:28:55 +0100

    Changes: Proper title set

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,15 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 {
 #ifdef __arm__
     QApplication *app = MDeclarativeCache::qApplication(argc, argv);
-    app->setProperty("NoMStyle", true);
     QDeclarativeView *view = MDeclarativeCache::qDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->showFullScreen();
+    view->setWindowTitle("Maps");
     QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
 
     return app->exec();
 #else
     QApplication app(argc, argv);
-    app.setProperty("NoMStyle", true);
     QDeclarativeView *view = new QDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->show();
