037d7d0c4746095e068906971185f212af74aee6
Jakub Pavelek <jpavelek@live.com>
2011-12-01 10:50:39 +0100

    Changes: Flicker free

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,22 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     QDeclarativeView *view = MDeclarativeCache::qDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->showFullScreen();
-    view->setWindowTitle("Maps");
-    QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
-
-    return app->exec();
 #else
-    QApplication app(argc, argv);
+    QApplication *app = new QApplication(argc, argv);
     QDeclarativeView *view = new QDeclarativeView();
     view->setSource(QUrl("qrc:/qml/main.qml"));
     view->show();
+#endif
+
+    view->setWindowTitle("Maps");
+    //Flickr removal
+    view->setAttribute(Qt::WA_OpaquePaintEvent);
+    view->setAttribute(Qt::WA_NoSystemBackground);
+    view->viewport()->setAttribute(Qt::WA_OpaquePaintEvent);
+    view->viewport()->setAttribute(Qt::WA_NoSystemBackground);
+
     QObject::connect(view->engine(), SIGNAL(quit()), view, SLOT(close()));
 
-    return app.exec();
-#endif
+    return app->exec();
 
 }
